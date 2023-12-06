4960c04ebc26834098aafc6d7c709dd8d5669482
sadia <saimislam64@gnail.com>
2023-12-06 17:45:39 +0600

    content update

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -13,141 +13,137 @@ import img14 from '../../images/img14.png';
 
 const Main = () => {
     return (
-       <>
-       <h5 class="text-center works">How Rushan Works</h5>
+        <>
+            <h5 class="text-center works">How Rushan Works</h5>
 
-        <p class="text-center">Save you time, money and energy and start to washing with us</p>
+            <p class="text-center">Save you time, money and energy and start to washing with us</p>
 
-        <section class="d-flex mt-5 pt-3 justify-content-evenly gap-5 " id='choose'>
-            <div>
-            <img src={img11.src} alt="" />   
-            </div>
-            <div class='choose-text'>
-                <h5 class="pt-5">Choose a Service</h5>
-                <p class="pt-2">Save you time, money and energy and start to washing with us,
-                    How to Order?</p>
-
+            <section class="d-flex mt-5 pt-3 justify-content-evenly gap-5 " id='choose'>
                 <div>
-                    {/* <i class="fa-sharp fa-solid fa-square-check rushan-color"></i> */}
-                    <label class="form-check-label" for="flexCheckDefault">
-                        Download App
-                    </label>
+                    <img src={img11.src} alt="" />
                 </div>
-                <div>
-                    {/* <i class="fa-sharp fa-solid fa-square-check rushan-color"></i> */}
-                    <label class="form-check-label" for="flexCheckChecked">
-                        Find Categories
-                    </label>
-                </div>
-                <div>
-                    {/* <i class="fa-sharp fa-solid fa-square-check rushan-color"></i> */}
-                    <label class="form-check-label" for="flexCheckChecked">
-                        Start schedule your laundry
-                    </label>
-                </div>
+                <div class='choose-text'>
+                    <h5 class="pt-5">Choose a Service</h5>
+                    <p class="pt-2">Save you time, money and energy and start to washing with us,
+                        How to Order?</p>
 
-            </div>
-        </section>
-
-        <section class="d-flex mt-5 pt-3 px-5 mx-5 justify-content-evenly gap-5 "  id='schedule'>
-            <div>
-                <h5 class="pt-5">Schedule a pickup</h5>
-                <p class="pt-2 pe-5 me-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis quod voluptate
-                    perferendis soluta distinctio quas vel, ipsam atque aut reiciendis expedita itaque voluptas esse
-                    similique, beatae libero eum eligendi corporis!</p>
-            </div>
-
-            <div>
-            <img src={img9.src} alt="" />
-            </div>
-        </section>
-
-        <section class="d-flex mt-5 pt-3 justify-content-evenly gap-5 px-5" id='items'>
-
-            <div class="">
-            <img src={img10.src} alt="" />
-            </div>
-
-            <div id='get-items'>
-                <h5 class="pt-5">Get your clean Items</h5>
-                <p class="pt-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid impedit, repellat quia
-                    sit, repellendus rem consequatur distinctio modi fugit molestiae quod et, placeat commodi. A soluta
-                    error cumque voluptates aut!</p>
-
-
-
-                <button type="button" class="btn header-color text-white">Go to app <i
-                        class="fa-solid fa-arrow-right"></i></button>
-            </div>
-        </section>
-
-
-        <section class="py-2 my-3 bg-color">
-            <div class="py-5 text-center" id='service'>
-                 <img src={img6.src} alt="" />
-            </div>
-           <div className='text-center'>
-           <button type="button" class="btn header-color text-white">View More</button>
-           </div>
-        </section>
-
-         <section>
-            <section class="download">
-                <div class="mx-5 ps-5">
-                    <div id="app">
-                        <h4 class="pb-4">Download this app</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur voluptas temporibus
-                            deleniti suscipit. Perferendis aperiam mollitia ab, odit fugiat commodi sequi veniam dolore,
-                            aspernatur illo, eius velit? Fugit, illo reprehenderit.</p>
+                    <div>
+                        {/* <i class="fa-sharp fa-solid fa-square-check rushan-color"></i> */}
+                        <label class="form-check-label" for="flexCheckDefault">
+                            Download App
+                        </label>
                     </div>
-                   
-                    <img class="mt-3" src={img1.src} alt=""/>
-                </div>
-
-                  <div class="group-img text-center">
-                    <img width="45%" src={img7.src} alt=""/>
-
-                    <div id="bg-images">
-                        <img src={img2.src} alt=""/>
+                    <div>
+                        {/* <i class="fa-sharp fa-solid fa-square-check rushan-color"></i> */}
+                        <label class="form-check-label" for="flexCheckChecked">
+                            Find Categories
+                        </label>
                     </div>
-                </div>
+                    <div>
+                        {/* <i class="fa-sharp fa-solid fa-square-check rushan-color"></i> */}
+                        <label class="form-check-label" for="flexCheckChecked">
+                            Start schedule your laundry
+                        </label>
+                    </div>
 
+                </div>
             </section>
-        </section>  
-        <section class="pt-5">
-            <div class="bg-color pt-5">
+
+            <section class="d-flex mt-5 pt-3 px-5 mx-5 justify-content-evenly gap-5 " id='schedule'>
                 <div>
-                    <h2 class="text-center">Our Mission & Vision</h2>
-                    <p class="text-center mission">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit sed
-                        asperiores, harum placeat quae enim ratione quibusdam quo optio incidunt et error unde eum
-                        numquam
-                        suscipit ut autem, facilis perspiciatis!
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum placeat vero saepe dolores
-                        nesciunt laudantium rerum, sunt repudiandae laboriosam ipsam mollitia nisi deserunt atque!
-                        Ducimus
-                        corrupti praesentium nesciunt aut ea.
+                    <h5 class="pt-5">Schedule a pickup</h5>
+                    <p class="pt-2 pe-5 me-5">Are you tired of managing your laundry schedule? We Have got you covered! Schedule a pickup with just a few clicks and enjoy the convenience of our hassle-free laundry service. We pick up and deliver at your doorstep, ensuring a seamless experience.
+                    </p>
+                </div>
+
+                <div>
+                    <img src={img9.src} alt="" />
+                </div>
+            </section>
+
+            <section class="d-flex mt-5 pt-3 justify-content-evenly gap-5 px-5" id='items'>
+
+                <div class="">
+                    <img src={img10.src} alt="" />
+                </div>
+
+                <div id='get-items'>
+                    <h5 class="pt-5">Get your clean Items</h5>
+                    <p class="pt-2">Experience the joy of receiving freshly cleaned and neatly folded items right at your doorstep. Our dedicated team ensures that your laundry is handled with care and delivered promptly. Say goodbye to laundry hassles and hello to clean, crisp clothes!
                     </p>
 
 
-                    <div>
-                        <h4 class="text-center"><span>Our excellent works</span></h4>
+
+                    <button type="button" class="btn header-color text-white">Go to app <i
+                        class="fa-solid fa-arrow-right"></i></button>
+                </div>
+            </section>
+
+
+            <section class="py-2 my-3 bg-color">
+                <div class="py-5 text-center" id='service'>
+                    <img src={img6.src} alt="" />
+                </div>
+                <div className='text-center'>
+                    <button type="button" class="btn header-color text-white">View More</button>
+                </div>
+            </section>
+
+            <section>
+                <section class="download">
+                    <div class="mx-5 ps-5">
+                        <div id="app">
+                            <h4 class="pb-4">Download this app</h4>
+                            <p>Unlock the full potential of our laundry services by downloading our user-friendly app. With a few taps, you can schedule pickups, track your orders, and stay connected with our services. Download now and simplify your laundry routine!
+                            </p>
+                        </div>
+
+                        <img class="mt-3" src={img1.src} alt="" />
                     </div>
-                    <p class="text-center">Gallery and some inspiration for doing a good washer</p>
+
+                    <div class="group-img text-center">
+                        <img width="45%" src={img7.src} alt="" />
+
+                        <div id="bg-images">
+                            <img src={img2.src} alt="" />
+                        </div>
+                    </div>
+
+                </section>
+            </section>
+            <section class="pt-5">
+                <div class="bg-color pt-5">
+                    <div>
+                        <h2 class="text-center">Our Mission & Vision</h2>
+                        <p class="text-center mission">
+                            Mission:
+                            At Rushan Smart Laundry, our mission is to redefine the laundry experience by providing a convenient, reliable, and efficient service that adapts to your lifestyle. We aim to save you time and effort, allowing you to focus on what matters most.
+                            <br />
+                            Vision:
+                            Our vision is to be the leading laundry service provider, setting the standard for excellence in the industry. We strive to continuously innovate, exceed customer expectations, and contribute to a cleaner and more sustainable future.
+
+                        </p>
+
+
+                        <div>
+                            <h4 class="text-center"><span>Our excellent works</span></h4>
+                        </div>
+                        <p class="text-center">Gallery and some inspiration for doing a good washer</p>
+                    </div>
+
+                    <div class="py-5 text-center" id="mission-img">
+                        <img src={img12.src} alt="" />
+
+                        <img class="pb-5" src={img13.src} alt="" />
+                        <img src={img14.src} alt="" />
+
+                    </div>
                 </div>
-
-                <div class="py-5 text-center" id="mission-img">
-                    <img src={img12.src} alt=""/>
-
-                    <img class="pb-5" src={img13.src} alt=""/>
-                    <img src={img14.src} alt=""/>
-
-                </div>
-            </div>
-        </section>
+            </section>
 
 
 
-       </>
+        </>
     );
 };
 
--- src/components/terms/Terms.js
+++ src/components/terms/Terms.js
@@ -8,66 +8,146 @@ const TermsLayout = () => {
 
 
             <div class="px-5 mx-5">
+                <p>Welcome to our Rushan Smart Laundry. By accessing or using our website or mobile application (collectively, the Services), you agree to comply with and be bound by these Terms and Conditions. If you do not agree with these terms, please do not use our Services.
+                </p>
+                <div>
+                    <h4 class="pt-4">1. Service Overview</h4>
+                    <h6>1.1 Description:</h6>
+                    <ul>
+
+                        <li>
+                            Rushan Smart Laundry provides an online platform for users to place laundry orders and schedule delivery services through our mobile application or website.
+
+                        </li>
+                    </ul>
+                    <h6>1.2 User Accounts:</h6>
+                    <ul>
+                        <li>
+                            To use certain features of the Services, you may be required to create a user account. You are responsible for maintaining the confidentiality of your account information.
+
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h4 class="pt-4">2. Order and Payment</h4>
+                    <h6>2.1 Placing Orders:</h6>
+                    <ul>
+                        <li>
+                            By placing an order through our Services, you agree to provide accurate and complete information, including payment details.
+
+                        </li>
+                    </ul>
+                    <h6>2.2 Payment:</h6>
+                    <ul>
+                        <li>
+                            Payment for services must be made through the approved payment methods provided on our platform.
+
+                        </li>
+                    </ul>
+                    <h6>2.3 Order Confirmation:</h6>
+                    <ul>
+                        <li>
+                        An order confirmation will be sent to your registered email address or phone number upon successful order placement
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h4 class="pt-4">3. Delivery and Returns</h4>
+                    <h6>3.1 Delivery:</h6>
+                    <ul>
+                        <li>
+                        We aim to deliver orders within the specified time frames. Delays may occur due to unforeseen circumstances, and we will strive to notify you of any changes.
+
+                        </li>
+                    </ul>
+                    <h6>3.2 Returns:</h6>
+                    <ul>
+                        <li>
+                        For issues with the quality of service, please contact our customer support within a reasonable time frame for resolution.
+
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h4 class="pt-4">4. User Responsibilities</h4>
+                    <h6>4.1 Prohibited Activities:</h6>
+                    <ul>
+                        <li>
+                        Users must not engage in any unlawful, fraudulent, or unauthorized activities when using our Services.
+                        </li>
+                    </ul>
+                    <h6>4.2 User Content:</h6>
+                    <ul>
+                        <li>
+                        Any content, including reviews and feedback, submitted by users must be accurate, lawful, and not violate the rights of others.
 
 
-                <h4 class="pt-4">Information Collection and Use</h4>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h4 class="pt-4">5. Intellectual Property</h4>
+                    <h6>5.1 Ownership:</h6>
+                    <ul>
+                        <li>
+                        All intellectual property rights related to the Services, including trademarks, copyrights, and logos, are owned by Rushan Smart Laundry.
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h4 class="pt-4">6. Termination</h4>
+                    <h6>6.1 Termination by Us:</h6>
+                    <ul>
+                        <li>
+                        We reserve the right to terminate or suspend your account and access to the Services at our discretion, without notice, for any reason, including violation of these Terms.
+                        </li>
+                    </ul>
+                </div>
 
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint animi exercitationem qui, earum quo
-                    corrupti eveniet voluptatibus alias natus harum dolores ullam maxime fugiat. Quo sint aspernatur labore
-                    a esse!L Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat soluta molestiae nulla nobis
-                    repellendus ex magnam dignissimos eaque id quasi? Accusamus ut repudiandae modi illum iure reiciendis
-                    quidem vitae esse. </p>
+                <div>
+                    <h4 class="pt-4">7. Limitation of Liability</h4>
+                    <h6>7.1 Disclaimer:</h6>
+                    <ul>
+                        <li>
+                        We do not guarantee the accuracy, completeness, or timeliness of information provided on the Services. Use the Services at your own risk.
 
-            </div>
+                        </li>
+                    </ul>
+                    <h6>7.2 Limitation of Liability:</h6>
+                    <ul>
+                        <li>
+                        To the fullest extent permitted by law, Rushan Smart Laundry shall not be liable for any direct, indirect, incidental, consequential, or punitive damages arising out of your use of the Services
 
-            <div class="px-5 mx-5">
-                <h4 class="pt-4">Information Sharing and Disclosure</h4>
+                        </li>
+                    </ul>
+                </div>
 
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint animi exercitationem qui, earum quo
-                    corrupti eveniet voluptatibus alias natus harum dolores ullam maxime fugiat. Quo sint aspernatur labore
-                    a esse!L Lorem ipsum dolor sit amet consectetur adipisicing elit.  </p>
+                <div>
+                    <h4 class="pt-4">8. Governing Law</h4>
+                    <h6>8.1 Jurisdiction:</h6>
+                    <ul>
+                        <li>
+                        These Terms and Conditions are governed by the laws of the United Arab Emirates. Any disputes shall be resolved in the appropriate courts in the UAE.
 
-            </div>
-            <div class="px-5 mx-5">
-                <h4 class="pt-4">Terms of Service, Notices, and Revisions</h4>
+                        </li>
+                    </ul>
+                </div>
 
-                <p><small>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint animi exercitationem qui, earum quo
-                    corrupti eveniet voluptatibus alias natus harum dolores ullam maxime fugiat. Quo sint aspernatur labore
-                    a esse!L Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint animi exercitationem qui, earum quo
-                    corrupti eveniet voluptatibus alias natus harum dolores ullam maxime fugiat. Quo sint aspernatur labore
-                    a esse!L Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                </small> </p>
-                <p><small>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint animi exercitationem qui, earum quo
-                    corrupti eveniet voluptatibus alias natus harum dolores ullam maxime fugiat. Quo sint aspernatur labore
-                    a esse!L Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint animi exercitationem qui, earum quo
-                    corrupti eveniet voluptatibus alias natus harum dolores ullam maxime fugiat. Quo sint aspernatur labore
-                    a esse!L Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                </small> </p>
+                <div>
+                    <h4 class="pt-4">9. Changes to Terms</h4>
+                    <h6>9.1 Modification:</h6>
+                    <ul>
+                        <li>
+                        We reserve the right to update or modify these Terms and Conditions at any time. The updated terms will be effective upon posting on our website or app.
 
-            </div>
+                        </li>
+                    </ul>
+                </div> 
+                <h6>Contact</h6>
+                <p>If you have any questions or concerns about these Terms and Conditions, please contact us at [insert contact email/phone number].
 
-
-            <div class="px-5 mx-5">
-                <h4 class="pt-4">Copyright</h4>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint animi exercitationem qui, earum quo
-                    corrupti eveniet voluptatibus alias natus harum dolores ullam maxime fugiat. Quo sint aspernatur labore
-                    a esse!L Lorem ipsum dolor sit amet consectetur adipisicing elit.  </p>
-            </div>
-            <div class="px-5 mx-5">
-                <h4 class="pt-4">Permitted Use</h4>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint animi exercitationem qui, earum quo
-                    corrupti eveniet voluptatibus alias natus harum dolores ullam maxime fugiat. Quo sint aspernatur labore
-                    a esse!L Lorem ipsum dolor sit amet consectetur adipisicing elit.  </p>
-            </div>
-            <div class="px-5 mx-5">
-                <h4 class="pt-4">Restricted Use</h4>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint animi exercitationem qui, earum quo
-                    corrupti eveniet voluptatibus alias natus harum dolores ullam maxime fugiat. Quo sint aspernatur labore
-                    a esse!L Lorem ipsum dolor sit amet consectetur adipisicing elit.  </p>
-            </div>
-
+</p>
+       </div>
         </>
     );
 };
